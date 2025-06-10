1a1b0832cca532c0202f0f992647b2bc07e1acc9
MontserratCL <a01713040@tec.mx>
2025-06-10 17:07:08 -0600

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -5,13 +5,14 @@
 #include <string>
 using namespace std;
 
+//Patient class inherits from Person and contains additional medical information such as blood type and allergies.
 class Patient : public Person {
 private:
     string bloodType;
     string allergies;
 
 public:
-    // Constructors
+    // Constructors (shows the method overloading)
     Patient(string name, string id, string phone, string bloodType);
     Patient(string name, string id, string phone, string bloodType, string allergies);
     
@@ -19,12 +20,18 @@ public:
     string getBloodType();
     string getAllergies();
     
-    // Methods
+    // Setter
+    void setAllergies(string allergies);
+    
+    // Methods (method overloading)
     void updateInfo(string bloodType);
     void updateInfo(string bloodType, string allergies);
     
-    // Pure virtual funct from person
+    // Pure virtual function from Person (that is the method overriding)
     string getInfo() override;
+    
+    // Operator overloading
+    string operator+(Patient& other);
 };
 
 #endif
